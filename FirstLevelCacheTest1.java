b39f9bbc8595bb1a15dc5e841af24b88f6fa708e
prateekbanra7 <66116227+prateekbanra7@users.noreply.github.com>
2025-05-07 14:57:16 +0530

    Add files via upload

--- FirstLevelCacheTest1.java
+++ FirstLevelCacheTest1.java
@@ -1,7 +1,6 @@
 package in.abc.main;
 
 import org.hibernate.Session;
-import org.hibernate.Transaction;
 
 import in.abc.Model.InsurancePolicy;
 import in.abc.util.HibernateUtil;
@@ -11,41 +10,36 @@ public class FirstLevelCacheTest1 {
 	public static void main(String[] args) {
 
 		Session session = null;
-		boolean flag = false;
-		Transaction transaction = null;
 		InsurancePolicy policy = null;
 
 		session = HibernateUtil.getSession();
 
 		try {
-			policy = session.get(InsurancePolicy.class, 1L);
-
-			if (policy == null) {
-				System.out.println("Record not found...");
-			} else {
-				System.out.println("Record found and displayed...");
-				System.out.println(policy);
-				transaction = session.beginTransaction();
-				policy.setTenure(9);
-				session.update(policy);
-
-				policy.setTenure(30);
-				session.update(policy);
-
-				flag = true;
-			}
+			policy = session.get(InsurancePolicy.class, 1L);//gets from DB and put it in L1-cache
+			System.out.println("1::" + policy);
+			
+			policy = session.get(InsurancePolicy.class, 1L);//gets from L1-cache
+			System.out.println("2::" + policy);
+			System.out.println("===========================");
+			
+			session.evict(policy);//remove policy object from L1 cache
+			
+			policy = session.get(InsurancePolicy.class, 1L);//gets from DB and put it in L1-cache
+			System.out.println("3::" + policy);
+			
+			policy = session.get(InsurancePolicy.class, 1L);//gets from L1 cache
+			System.out.println("4::" + policy);
+			System.out.println("===========================");
+			
+			session.clear();//remove all objects from L1-cache
+			
+			policy = session.get(InsurancePolicy.class, 1L);//gets from DB and put it in L1-cache
+			System.out.println("5::" + policy);
 
 		} catch (Exception e) {
 			e.printStackTrace();
-			flag = false;
 		} finally {
-			if (flag) {
-				transaction.commit();
-				System.out.println("Object updated...");
-			} else {
-				transaction.rollback();
-				System.out.println("Object not updated...");
-			}
+			
 			HibernateUtil.closeSession(session);
 		}
 	}
